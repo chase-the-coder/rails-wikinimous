34270f01bb91a1cfd6f5a9c42d7dc2d28d6ec46a
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-20 19:06:57 -0400

    created crud and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require 'faker'
 puts 'destroying seeds'
 Article.destroy_all
 puts 'Seeding...'
-10.times { Article.create(title: Faker::GreekPhilosophers.quote, content: Faker::GreekPhilosophers.name) }
+10.times { Article.create(title: Faker::GreekPhilosophers.quote, content: Faker::Lorem.sentence(word_count: 35)) }
 puts 'Seeding complete!'
