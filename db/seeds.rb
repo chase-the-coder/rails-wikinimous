68f9d92e78c736c8a64b6aaa812be1498496d124
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-20 14:37:57 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 require 'faker'
-
-10.times { Article.new(title: Faker::GreekPhilosophers.quote, content: Faker::GreekPhilosophers.name) }
+puts 'destroying seeds'
+Article.destroy_all
+puts 'Seeding...'
+10.times { Article.create(title: Faker::GreekPhilosophers.quote, content: Faker::GreekPhilosophers.name) }
+puts 'Seeding complete!'
